1c6996621f9fc7f7db8188ae8e5a9e44ee2e190f
abolabo <abolabo@gmail.com>
2025-03-25 13:50:16 +0200

    core: AView. Path fix

--- public_html/core/engine/view.php
+++ public_html/core/engine/view.php
@@ -80,7 +80,7 @@ class AView
                 : $this->registry->get('config')->get('config_storefront_template');
         }
         $this->data['template_dir'] = RDIR_TEMPLATE;
-        $this->data['tpl_common_dir'] = RDIR_TEMPLATE.'/template/common/';
+        $this->data['tpl_common_dir'] = RDIR_TEMPLATE.'template/common/';
         $this->instance_id = $instance_id;
 
     }
