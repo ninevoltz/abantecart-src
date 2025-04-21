a80723c736392bbd3da115873a8cda89a7066bb2
abolabo <abolabo@gmail.com>
2025-04-21 21:13:59 +0300

    novator: category_slides block fix. ACORN-1591

--- public_html/extensions/novator/storefront/controller/blocks/category_slides.php
+++ public_html/extensions/novator/storefront/controller/blocks/category_slides.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ *   $Id$
+ *
+ *   AbanteCart, Ideal OpenSource Ecommerce Solution
+ *   http://www.AbanteCart.com
+ *
+ *   Copyright © 2011-2025 Belavier Commerce LLC
+ *
+ *   This source file is subject to Open Software License (OSL 3.0)
+ *   License details is bundled with this package in the file LICENSE.txt.
+ *   It is also available at this URL:
+ *   <http://www.opensource.org/licenses/OSL-3.0>
+ *
+ *  UPGRADE NOTE:
+ *    Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+ *    versions in the future. If you wish to customize AbanteCart for your
+ *    needs please refer to http://www.AbanteCart.com for more information.
+ */
 
 class ControllerBlocksCategorySlides extends AController
 {
@@ -30,10 +48,12 @@ class ControllerBlocksCategorySlides extends AController
             }
             $categoryId = array_pop($parts);
         } else {
-            $categoryId = 0;
+            $categoryId = !is_array($this->request->get['category_id'])
+                ? (int)$this->request->get['category_id']
+                : (int)current($this->request->get['category_id']);
         }
         $resources = [];
-        if($categoryId) {
+        if ($categoryId) {
             $AResource = new AResource('image');
             $resources = $AResource->getResourceAllObjects(
                 'categories',
@@ -41,7 +61,7 @@ class ControllerBlocksCategorySlides extends AController
                 [
                     'main' => [
                         'height' => 350,
-                        'width' => 320
+                        'width'  => 320
                     ]
                 ]
             );
